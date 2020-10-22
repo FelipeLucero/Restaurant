b6fe25e95bf7ff56a1536f2c0d327a4669eb335a
FelipeLucero <45726971+FelipeLucero@users.noreply.github.com>
2020-10-22 18:13:44 -0300

    recuperar contraseña para usuarios

--- Restaurant/settings.py
+++ Restaurant/settings.py
@@ -125,8 +125,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '' 
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'restaurantsigloo21' 
+EMAIL_HOST_PASSWORD = 'Caramelo1994'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
