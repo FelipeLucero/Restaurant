71a16dad83c54b1edf2f6cc40d7306fdd74c2e5e
FelipeLucero <45726971+FelipeLucero@users.noreply.github.com>
2020-10-29 02:18:35 -0300

    version 2 registro

--- app_Restaurant/static/js/validacion2.js
+++ app_Restaurant/static/js/validacion2.js
@@ -1,47 +1,41 @@
 function validacionesFormulario(){
     
-    let nombres, apellidos, usuario,email, telefono, contraseña1,contraseña2
-    nombres = document.getElementById("first_name").value;
-    apellidos = document.getElementById("last_name").value;
-    usuario = document.getElementById("username").value;
-    email = document.getElementById("email").value;
-    telefono = document.getElementById("phone").value;
-    contraseña1= document.getElementById("password1").value;
-    contraseña2 = document.getElementById("password2").value;
-    let espacios = false;
-    let cont = 0;
+  let  usuario,email, contraseña1,contraseña2
+  usuario = document.getElementById("username").value;
+  email = document.getElementById("email").value;
+  contraseña1= document.getElementById("password1").value;
+  contraseña2 = document.getElementById("password2").value;
+  let espacios = false;
+  let cont = 0;
+ 
+  if( usuario == ""|| email == "" || contraseña1 == "" || contraseña2 == "" ){
+
+    swal("Advertencia", "...todos los campos son obligatorios", "warning");
+  
+    return false;
+  } 
+
+  while (!espacios && (cont < contraseña1.length)) {
+    if (contraseña1.charAt(cont) == " ")
+      espacios = true;
+    cont++;
+  }
    
-  
-  
-  
-    if(nombres == "" || apellidos == ""|| usuario == ""|| email == "" || telefono =="" ||contraseña1 == "" || contraseña2 == "" ){
-  
-      swal("Advertencia", "...todos los campos son obligatorios", "warning");
-    
+  if (espacios) {
+                        
+  swal("Advertencia", "...La contraseña no puede contener espacios en blanco", "warning");
+      
+    return false;
+  }
+
+  if (contraseña1 != contraseña2) {
+                            
+  swal("Advertencia", "...Las Contraseñas deben de coincidir", "warning");
+      
       return false;
-    } 
-  
-    while (!espacios && (cont < contraseña1.length)) {
-      if (contraseña1.charAt(cont) == " ")
-        espacios = true;
-      cont++;
-    }
-     
-    if (espacios) {
-        	        				
-		swal("Advertencia", "...La contraseña no puede contener espacios en blanco", "warning");
-        
-      return false;
-    }
-  
-    if (contraseña1 != contraseña2) {
-        			        				
-		swal("Advertencia", "...Las Contraseñas deben de coincidir", "warning");
-        
-        return false;
-      } else {
-        swal("Excelente", "Registro exitoso");	
-        return true; 
-    }
-  
+    } else {
+      swal("Excelente", "Registro exitoso");	
+      return true; 
+  }
+
 }
